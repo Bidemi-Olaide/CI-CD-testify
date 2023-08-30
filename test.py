75b8d050863ca96c00f2f89fe51203b481f20bf0
Bidemi Olaide <haramidey239@gmail.com>
2023-08-30 23:16:53 +0100

    add test for  subtraction

--- test.py
+++ test.py
@@ -9,5 +9,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(main.addition(5, 5), 10, "Should be 10")
         self.assertEqual(main.addition(40, 20), 60, "Should be 60")
         self.assertEqual(main.addition(-3, 2),-1,"Should be -1")
+    def test_subraction(self):
+        self.assertEqual(main.subtraction(5,4), 1, "Should be 1")
 if __name__ == '__main__':
     unittest.main()
